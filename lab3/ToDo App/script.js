c1f34c1fb25d5f3504e3919e7a375d0f59397651
Tomeris Meshitbay <98958637+tomerismeshitbai@users.noreply.github.com>
2023-02-16 12:42:03 +0600

    add action:adding new item

--- lab3/ToDo App/script.js
+++ lab3/ToDo App/script.js
@@ -2,5 +2,17 @@ document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please,enter smth")
     }
- 
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+        document.querySelector("#newtask input").value = "";
+    }
 }
