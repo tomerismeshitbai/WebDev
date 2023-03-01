672e6fd3b4c75d8c700a747efb0db9d198631ce1
Tomeris Meshitbay <98958637+tomerismeshitbai@users.noreply.github.com>
2023-03-01 20:54:21 +0600

    ProductList, ProductItem,show list of categories

---
+++ lab5/src/app/product-item/product-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent {
+  products = products;
+  @Input() selectedproduct: string | undefined;
+
+  share(a : string) {
+    window.open("https://t.me/share/url?url=http://localhost:4200/&text="+a);
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
+}
---
+++ lab5/src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  selectedProduct?:string;
+
+ onSelect(a:string){
+  this.selectedProduct=a;
+ }
+ share(a : string) {
+  window.open("https://t.me/share/url?url=http://localhost:4200/&text="+a);
+}
+
+onNotify() {
+  window.alert('You will be notified when the product goes on sale');
+}
+}
