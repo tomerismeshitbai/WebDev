ef709effdc52eb11e295f5dd002c2362e7a2ee5d
Tomeris Meshitbay <98958637+tomerismeshitbai@users.noreply.github.com>
2023-03-08 20:48:19 +0600

    finishing

--- Weblab6/src/app/album-photos/album-photos.component.ts
+++ Weblab6/src/app/album-photos/album-photos.component.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Album, Photo} from "../models";
 import {AlbumsService} from "../albums.service";
-
 @Component({
   selector: 'app-album-photos',
   templateUrl: './album-photos.component.html',
   styleUrls: ['./album-photos.component.css']
 })
-export class AlbumPhotosComponent {
 export class AlbumPhotosComponent implements OnInit{
   album: Album;
+  albumID: number;
   loaded : boolean;
   photos: Photo[];
   constructor(private route: ActivatedRoute, private albumService : AlbumsService) {
     this.album = {} as Album;
+    this.albumID = 0;
     this.photos = [];
     this.loaded = true;
   }
@@ -31,9 +30,14 @@ export class AlbumPhotosComponent implements OnInit{
   }
 
   getPhotos(){
+    this.route.paramMap.subscribe((params) => {
+      this.albumID = Number(params.get('id'));
+    });
+
     this.loaded = false;
 
     this.albumService.getPhotos(this.album.id).subscribe((photos) =>{
+    this.albumService.getPhotos(this.albumID).subscribe((photos) =>{
       this.photos = photos;
     })
 
