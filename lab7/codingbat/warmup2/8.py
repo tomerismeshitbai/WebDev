b180ef8f65102e29e651cf74d197a80bf9717b74
Tomeris Meshitbay <98958637+tomerismeshitbai@users.noreply.github.com>
2023-03-26 20:29:22 +0600

    string

---
+++ lab7/codingbat/warmup2/8.py
@@ -0,0 +1,6 @@
+def array123(nums):
+  # iterating with length-2, to use i+1 and i+2 in the loop
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False
