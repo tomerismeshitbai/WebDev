34e8bdb9a78580ecbf715dadc42f65aeab638200
Tomeris Meshitbay <98958637+tomerismeshitbai@users.noreply.github.com>
2023-04-05 20:26:28 +0600

    step2:Creating models for database

--- lab8/shop_back/api/models.py
+++ lab8/shop_back/api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField(null=True)
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
+
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.to_json()
+        }
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'name': self.name
+        }
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
